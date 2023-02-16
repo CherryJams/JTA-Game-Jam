4b195fee54817707faa0ec43e3208be1640d58f8
Giovanni Braconi <thumpback2002.it@gmail.com>
2023-02-16 12:36:55 +0100

    Chaged dash input to J

--- Assets/MetroidvaniaController/Scripts/Player/PlayerMovement.cs
+++ Assets/MetroidvaniaController/Scripts/Player/PlayerMovement.cs
@@ -22,12 +22,12 @@ public class PlayerMovement : MonoBehaviour {
 
 		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-		if (Input.GetKeyDown(KeyCode.Z))
+		if (Input.GetKeyDown(KeyCode.W))
 		{
 			jump = true;
 		}
 
-		if (Input.GetKeyDown(KeyCode.C))
+		if (Input.GetKeyDown(KeyCode.J))
 		{
 			dash = true;
 		}
